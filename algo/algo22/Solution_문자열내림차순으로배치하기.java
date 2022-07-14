bda5fee81c280b9e4ca076a15be67c3d12bd6811
Kimsihyo <kczxc@naver.com>
2022-07-14 17:33:30 +0900

    프로그래머스 정수내림자순으로배치하기

--- algo/algo22/Solution_문자열내림차순으로배치하기.java
+++ algo/algo22/Solution_문자열내림차순으로배치하기.java
@@ -7,29 +7,31 @@ import java.util.PriorityQueue;
 public class Solution_문자열내림차순으로배치하기 {
     
 	public static void main(String[] args) {
-		solution("Zbcdefg");
+		solution(118372);
 	}
 
-	public static String solution(String s) {
-        String answer = "";
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-        	
-        	@Override
-        	public int compare(Integer o1, Integer o2) {
-        		// TODO Auto-generated method stub
-        		return o2-o1;
-        	}
-        	
-		});
-        char[] temp = s.toCharArray();
-        for(int i=0; i<temp.length; i++) {
-        	pq.add(temp[i] - '0');
-        }
-        
-        for(int i=0; i<temp.length; i++) {
-        	temp[i] = (char) (pq.poll() + '0');
-        }
-        System.out.println(temp);
-        return answer;//"gfedcbZ"
+	 public static long solution(long n) {
+	        long answer = 0;
+	        String string_N = String.valueOf(n);
+	        char[] temp = string_N.toCharArray();
+	        PriorityQueue<Integer> saveNumber 
+	            = new PriorityQueue<>(new Comparator<Integer>(){
+	                
+	                @Override
+	                public int compare(Integer o1, Integer o2){
+	                    return o2 - o1;
+	                }
+	        });
+	        for(int i=0; i<temp.length; i++){
+	            saveNumber.add(temp[i] - '0');
+	        }
+	        String tempAnswer = "";
+	        for(int i=0; i<temp.length; i++){
+	            int tempI = saveNumber.poll();
+	            String tempS = String.valueOf(tempI);
+	            tempAnswer += tempS;
+	        }
+	        answer = Long.valueOf(tempAnswer);
+	        return answer;
 	}
 }
---
+++ algo/algo22/Solution_정수내림차순으로배치하기.java
@@ -0,0 +1,35 @@
+package algo22;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class Solution_정수내림차순으로배치하기 {
+    
+	public static void main(String[] args) {
+		solution("Zbcdefg");
+	}
+
+	public static String solution(String s) {
+        String answer = "";
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+        	
+        	@Override
+        	public int compare(Integer o1, Integer o2) {
+        		// TODO Auto-generated method stub
+        		return o2-o1;
+        	}
+        	
+		});
+        char[] temp = s.toCharArray();
+        for(int i=0; i<temp.length; i++) {
+        	pq.add(temp[i] - '0');
+        }
+        
+        for(int i=0; i<temp.length; i++) {
+        	temp[i] = (char) (pq.poll() + '0');
+        }
+        System.out.println(temp);
+        return answer;//"gfedcbZ"
+	}
+}
