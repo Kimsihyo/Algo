ce4e6fc90bc949bff5d247ecabbaa262f3c050f4
Kimsihyo <kczxc@naver.com>
2022-07-14 17:22:58 +0900

    프로그래머스 자릿수더하기

--- algo/algo22/Solution_자릿수더하기.java
+++ algo/algo22/Solution_자릿수더하기.java
@@ -6,19 +6,28 @@ import java.util.PriorityQueue;
 public class Solution_자릿수더하기 {
 
 	public static void main(String[] args) {
-		int n = 123;
+		int[] n = {4,3,2,1};
 		solution(n);
 	}
 
-	public static int solution(int n) {
-		int answer = 0;
-		String stringNum = String.valueOf(n);
-		char[] number = stringNum.toCharArray();
-		for(int i=0; i<number.length; i++) {
-//			System.out.println(number[i]);
-			answer += (number[i] - '0');
-		}
-//		System.out.println(answer);
+	public static int[] solution(int[] arr) {
+        if(arr.length == 1){
+            int[] answer = {-1};
+            return answer;
+        }
+        int[] answer = new int[arr.length-1];
+        int min = 999999999;
+        for(int i=0; i<arr.length; i++){
+            min = Math.min(min, arr[i]);
+            //System.out.println(min);
+        }
+        int idx = 0;
+        for(int i=0; i<arr.length; i++){
+            if(min != arr[i]){
+                answer[idx] = arr[i];
+                idx++;
+            }
+        }
         return answer;
 	}
 }
